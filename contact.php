5204735ae56fc57549cde3099ddcb2eb9008a66d
Huachen <huachenw24@gmail.com>
2021-05-21 22:36:01 +0800

    add contact page

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php
+    include 'header.php';
+?>
+
+<p class='collab'>Wish to collaborate with us?</p>
+<p class='collab-sub'>Drop us an email at<br>collab@mindsitesg.org</p>
+<img class='mascots' src='images/mascots_all.png'>
+<a class='email-us' href='mailto: collab@mindsitesg.org'>Email Us</a>
+
+</div>
+</body>
+</html>
