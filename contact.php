9786004b86751eb2966467f5b868422db82667ee
Huachen <huachenw24@gmail.com>
2021-05-21 23:14:07 +0800

    standardize contact us styling

--- contact.php
+++ contact.php
@@ -2,8 +2,8 @@
     include 'header.php';
 ?>
 
-<p class='collab'>Wish to collaborate with us?</p>
-<p class='collab-sub'>Drop us an email at<br>collab@mindsitesg.org</p>
+<p class='form-header'>Wish to collaborate with us?</p>
+<p class='form-subheader'>Drop us an email at<br>collab@mindsitesg.org</p>
 <img class='mascots' src='images/mascots_all.png'>
 <a class='email-us' href='mailto: collab@mindsitesg.org'>Email Us</a>
 
