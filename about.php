6190d5861742b6eddbf9e007e5500ebadcdec197
gureccheese <79566646+gureccheese@users.noreply.github.com>
2021-05-21 12:03:14 +0800

    Changed about description and added headers
    to add images later

--- about.php
+++ about.php
@@ -3,11 +3,17 @@
 ?>
 
 <div class="content">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque, eros vel facilisis vehicula, neque dui commodo ex, vel pulvinar mi enim in quam. Nam nisi est, molestie porta quam quis, scelerisque tristique ligula. Aliquam et leo porta, porttitor velit nec, mollis velit. Maecenas a hendrerit diam. Proin feugiat dolor in augue efficitur aliquam. In scelerisque orci et tristique finibus. Duis aliquam in tellus at ornare. In a tempus quam, sollicitudin sagittis eros. Ut in porta ligula, at viverra lorem.</p>
+    <h2> About MindSite </h2>
 
-    <p>Proin laoreet semper ligula, sit amet faucibus leo rhoncus dignissim. Vivamus sagittis lectus magna, vitae ultrices sem interdum in. Nam fermentum velit vel mi blandit, at cursus odio porttitor. Sed vestibulum elit augue. Etiam accumsan mattis lacus. Nunc sed viverra purus. Fusce et consectetur nibh, id ullamcorper dui. Aliquam erat volutpat. Nunc dolor eros, molestie vitae velit non, fermentum ultrices lectus. Mauris magna erat, congue dignissim interdum eu, tincidunt vel massa. Aliquam sit amet risus porta ex eleifend mollis sollicitudin vitae leo. Suspendisse tincidunt metus et enim egestas, nec scelerisque nibh pretium. Nunc sit amet ullamcorper turpis. Maecenas vestibulum nulla vel enim egestas lobortis. Morbi consequat justo aliquam, faucibus est eu, egestas lorem.</p>
+    <p>Upon seeing Singaporean Youth struggle to sieve through pages of Mental Wellness resources, a group of students sought to create a solution for greater access to Mental Wellness. And thus, MindSite was born! In partnership with Strengthening Youths in a Network of Care (SYNC), MindSite seeks to provide a filterable database of verified mental wellness resources tailored to each individual's needs.</p>
+
+    <h2> Our Mission </h2>
+
+    <p>MindSite is a platform that eases Youths’  search for Mental Health resources, all while keeping in mind each youth’s particular needs.<br> We aim to empower youths to seek the mental support they need easily.</p>
+    
+    <h2>About Mindfullness</h2>
         
-    <p>Vivamus sit amet porta arcu, non vehicula erat. Nam eget mauris laoreet, fermentum dolor vitae, accumsan risus. Pellentesque eget odio magna. Suspendisse ut mattis massa. Praesent viverra feugiat venenatis. Nunc eu leo porta, luctus dolor sit amet, placerat libero. Morbi pulvinar dui sit amet nunc commodo, sagittis fermentum velit facilisis. Vestibulum tempus nisi et vestibulum molestie. Nulla rutrum, libero et malesuada tincidunt, nunc nisl eleifend orci, ac ornare tortor diam vulputate dolor.</p>
+    <p>One of SYNC's five DBT Monsters. They are focused,open-minded and manifest big brain energy. However, they do get a little sleepy sometimes</p>
 </div>
   
 </div>
