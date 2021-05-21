da25506969621e8830e9f42f7b700638596fbde0
gureccheese <79566646+gureccheese@users.noreply.github.com>
2021-05-21 23:00:24 +0800

    minor adjustments to paragraph content

--- about.php
+++ about.php
@@ -5,17 +5,17 @@
 <div class="content">
     <h2> About MindSite </h2>
 
-    <p>Upon seeing Singaporean Youth struggle to sieve through pages of Mental Wellness resources, a group of students sought to create a solution for greater access to Mental Wellness. And thus, MindSite was born! In partnership with Strengthening Youths in a Network of Care (SYNC), MindSite seeks to provide a filterable database of verified mental wellness resources tailored to each individual's needs.</p>
+    <p>Upon seeing Singaporean Youth struggle to sieve through pages of Mental Wellness resources, a group of students sought to create a solution for greater access to Mental Wellness. And thus, MindSite was born! <br></br>In partnership with Strengthening Youths in a Network of Care (SYNC), MindSite seeks to provide a filterable database of verified mental wellness resources tailored to each individual's needs.</p>
 
     <h2> Our Mission </h2>
 
-    <p>MindSite is a platform that eases Youths’ search for Mental Health resources, all while keeping in mind each youth’s particular needs.<br>We aim to empower youths to seek the mental support they need easily.</p>
+    <p>MindSite is a platform that eases Youths’ search for Mental Health resources, all while keeping in mind each youth’s particular needs.<br></br>We aim to empower youths to seek the mental support they need easily.</p>
     
     <h2>About Mindfullness</h2>
         
     <div class='about-mindfullness'>
         <img class='mindfullness' src='images/mascot_mindfullness.png'>
-        <p>One of SYNC's five DBT Monsters. They are focused,open-minded and manifest big brain energy.<br>However, they do get a little sleepy sometimes.</p>
+        <p>One of SYNC's five DBT Monsters.They are focused, open-minded and manifest big brain energy.</br> </br> However, they do get a little sleepy sometimes.</p>
     </div>
     
 </div>
